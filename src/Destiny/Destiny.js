ef473669e5f9f6b21c68522c8271b76eabd71b91
Devon Bull <dbull@live.com>
2017-07-22 23:33:48 -0600

    experiencing webpack problems but destiny component created

---
+++ src/Destiny/Destiny.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Destiny extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Lone Wolf</div>
+        <div>War Room</div>
+      </div>
+    )
+  }
+}
+
+export default Destiny
---
+++ src/Destiny/DestinyContainer.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux'
+import Destiny from './Destiny'
+
+const mapStateToProps = (state) => {
+  return state
+}
+
+export default connect(mapStateToProps, null)(Destiny)
