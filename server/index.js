5b11f507d185dc98929c5654a4915c4970db681b
Chris Jordan <cjorda15@msudenver.edu>
2017-07-23 12:53:59 -0600

    connected random users for battlemode with sockets

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const routes = require('./routes')
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
+const socketDb =  require('./socketDB')
 
 app.use(bodyParser.json())
 
@@ -30,9 +31,26 @@ io.on('connection', function(socket){
   console.log("socket id has connected", socket.id)
   console.log('user total after connection', io.engine.clientsCount)
 
+  socket.emit("userConnection","HELLLO")
+
   socket.on('disconnect', function () {
     console.log("socket id has disconncted", socket.id)
     console.log("user total after disconnect", io.engine.clientsCount)
+  })
 
+  socket.on('logged in', (msg) => {
+    socketDb.users[msg.username] = socket.id
+  })
+
+  socket.on('random match request', (msg) => {
+    console.log(msg,"message from random request by client socket")
+      if(socketDb.randomMatches.length>0){
+        io.sockets.connected[socketDb.randomMatches[0].socket].emit('connected random 1v1', msg)
+        io.sockets.connected[socketDb.users[msg]].emit('connected random 1v1',socketDb.randomMatches[0].username)
+      }else{
+      socketDb.randomMatches.push({username:msg, socket:socketDb.users[msg]})
+      io.sockets.connected[socketDb.users[msg]].emit('awaiting random 1v1',"wait until another user joins random mathches")
+
+    }
   })
 });
