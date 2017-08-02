f0f399b6775afbeaecfeb8e06fba83b7ba7c8ccd
Devon Bull <dbull@live.com>
2017-08-01 23:48:11 -0600

    setuping up tests

--- src/Containers/CreateChallenge/CreateChallenge.test.js
+++ src/Containers/CreateChallenge/CreateChallenge.test.js
@@ -4,6 +4,10 @@ import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import fetchMock from 'fetch-mock'
 import { browserHistory } from 'react-router-dom'
-import NewTest from './NewTest'
-// import CreateChallengeContainer from './CreateChallengeContainer';
+import CreateChallengeContainer from './CreateChallengeContainer';
 import CreateChallenge from './CreateChallenge'
+
+describe('CreateChallengeContainer', () => {
+  const mockStore = configureMockStore()({})
+  
+})
